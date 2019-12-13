121714044733ea25f6a70fe12683aa554d2fb89d
Valeriya <56223050+ValeriyZolotoskaya@users.noreply.github.com>
2019-12-13 17:41:05 +0300

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,12 @@
+package lab7;
+
+public class Tester {
+    public static void main(String[] args) {
+        Car c1 = new Car("Infiniti", "QX50", "S600", 7000000, 2014);
+        c1.println();
+        Car c2 = new Car("BMW", "5 Series", "550 Li", 75221, 2015);
+        c2.println();
+        Car c3 = new Car("Audi", "q7", "560", 7450000, 2003);
+        c3.println();
+    }
+}
